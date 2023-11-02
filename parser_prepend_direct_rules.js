b3b35a75eee8b5e211b5bfb179fa7c8ae9da456f
hect0x7 <93357912+hect0x7@users.noreply.github.com>
2023-11-02 16:09:19 +0800

    parser: prepend direct rules

---
+++ parser_prepend_direct_rules.js
@@ -0,0 +1,23 @@
+module.exports.parse = async (raw, { axios, yaml, notify, console }, { name, url, interval, selected }) => {
+    const obj = yaml.parse(raw)
+
+    const DIRECT = "DIRECT"
+    const domains = `
+    visualstudio.com
+    vsassets.io
+    default.exp-tas.com
+    elemecdn
+    ele.me
+    wechat
+
+    `
+    .split('\n')
+    .map(domain => domain.trim())
+    .filter(domain => domain !== "")
+
+    // 去除空行和注释行
+    const newRules = domains.map(domain => `DOMAIN-KEYWORD,${domain},${DIRECT}`)
+    obj.rules = [...newRules, ...obj.rules]
+
+    return yaml.stringify(obj)
+}
